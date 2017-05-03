62eebf862bff55085800f8bbc4765bd0202bbbdc
HashiCorp Training <training@hashicorp.com>
2017-05-03 23:14:08 +0000

    Initial commit

---
+++ main.tf
@@ -0,0 +1,61 @@
+#
+# DO NOT DELETE THESE LINES!
+#
+# Your AMI ID is:
+#
+#     ami-2df66d3b
+#
+# Your subnet ID is:
+#
+#     subnet-75a08610
+#
+# Your security group ID is:
+#
+#     sg-4cc10432
+#
+# Your Identity is:
+#
+#     testing-seahorse
+#
+
+terraform {
+  backend "atlas" {
+    name = "jaloare/training"
+  }
+}
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "${var.aws_region}"
+}
+
+resource "aws_instance" "web" {
+  count                  = 2
+  ami                    = "ami-2df66d3b"
+  instance_type          = "t2.micro"
+  subnet_id              = "subnet-75a08610"
+  vpc_security_group_ids = ["sg-4cc10432"]
+
+  tags {
+    Name     = "web ${count.index}/x"
+    Identity = "testing-seahorse"
+    key      = "value"
+    testing  = "seahorse"
+  }
+}
+
+output "public_ip" {
+  value = ["${aws_instance.web.*.public_ip}"]
+}
+
+output "public_dns" {
+  value = ["${aws_instance.web.*.public_dns}"]
+}
